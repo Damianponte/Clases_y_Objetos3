171d4f830900b67dda7ade0f19bc5dcc85c8684f
dami <damianponte@hotmail.es>
2026-01-19 23:27:24 +0100

    empezando ejercicio contador

---
+++ src/Contador.java
@@ -0,0 +1,10 @@
+public class Contador {
+    //Crea una clase Contador que tenga un atributo valor.
+    // Implementa un constructor que permita indicar el valor inicial.
+    // Añade métodos para incrementar el contador, decrementarlo y reiniciarlo a cero.
+    // El contador nunca puede tener valores negativos. Añade un método que muestre el valor actual.
+    //  Desde el main, prueba todas las operaciones.
+
+    
+
+}
