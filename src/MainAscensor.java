2a5c88fc650e533d3cf8d594e16f5c8eb37a9149
dami <damianponte@hotmail.es>
2026-01-20 13:53:46 +0100

    ejercicio main completo

--- src/MainAscensor.java
+++ src/MainAscensor.java
@@ -1,5 +1,33 @@
 public class MainAscensor {
     public static void main(String[] args) {
-        
+        Ascensor planta=new Ascensor(4);
+
+        System.out.println("el ascensor esta en la planta " + planta.getPlantaActual() );
+
+        System.out.println(" el ascensor ha subido una planta");
+               planta.aumentarPlanta();
+                planta.mostrarPlanta();
+        System.out.println(" el ascensor ha subido una planta");
+        planta.aumentarPlanta();
+        planta.mostrarPlanta();
+        System.out.println(" ");
+        System.out.println(" el ascensor esta bajando;");
+        planta.decrementarPlanta();
+        planta.mostrarPlanta();
+        System.out.println(" ");
+        System.out.println(" el ascensor esta bajando;");
+        planta.decrementarPlanta();
+        planta.mostrarPlanta();
+        System.out.println(" ");
+        System.out.println(" el ascensor esta bajando;");
+        planta.decrementarPlanta();
+        planta.mostrarPlanta();
+
+
+
+
+
+
+
     }
 }
