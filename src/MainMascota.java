40e02d6bc1d63207d226391ab2095a1601e87514
dami <damianponte@hotmail.es>
2026-01-20 12:34:24 +0100

    ejercicio completo,

---
+++ src/MainMascota.java
@@ -0,0 +1,5 @@
+public class MainMascota {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Mascota.java
+++ src/Mascota.java
@@ -1,2 +1,52 @@
 public class Mascota {
+    //Crea una clase Mascota con los atributos nombre y hambre.
+    // El valor de hambre debe estar entre 0 y 100.
+    // Implementa un constructor que inicialice la mascota.
+    // Añade un método comer que reduzca el hambre y otro método pasarTiempo que la aumente;
+    //  por ejemplo, cada vez que come se reduce su hambre en 25, y por cada hora que pasa se aumenta su hambre en 5.
+    //  Añade un método mostrarEstado que muestre el nombre y el nivel de hambre. Desde el main,
+    //   simula el paso del tiempo y la alimentación de la mascota.
+
+    private String nombre;
+    private int hambre;
+
+
+    public Mascota(int hambre, String nombre) {
+        this.hambre = hambre;
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getHambre() {
+        return hambre;
+    }
+
+    public void reducirHambre(){
+        hambre=hambre -25;
+
+        if (hambre<=0){
+        hambre=0;
+        System.out.println("no tiene hambre");
+        }
+    }
+    public void pasarTiempo(int horas) {
+        hambre += horas * 5;
+
+        if (hambre>=100){
+            hambre=100;
+        }
+    }
+
+    public void mostrarEstado(){
+        if (hambre==100){
+            System.out.println(getNombre() + " no tiene hambre esta lleno " + getHambre());
+        }else{
+            System.out.println(getNombre() + "  tiene " + getHambre() + " de hambre");
+        }
+
+    }
+
 }
