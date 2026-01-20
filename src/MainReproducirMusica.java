61a9a62db0cd26e9e1d04110cb6173c8f807452f
dami <damianponte@hotmail.es>
2026-01-20 18:24:06 +0100

    parte main completa y funcional

--- src/MainReproducirMusica.java
+++ src/MainReproducirMusica.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class MainReproducirMusica {
+    public static void main(String[] args) {
+        ReproducirMusica cancion =new ReproducirMusica(5);
+
+        cancion.MostrarEstado();
+        System.out.println("ha subido el volumen al maximo");
+        cancion.AumentarVolumen(100);
+        cancion.MostrarEstado();
+        System.out.println("como hacia tanto ruido vinieron y lo bajaron a 30");
+        cancion.descenderVolumen(70);
+        cancion.MostrarEstado();
+
+        
+
+
+    }
 }
