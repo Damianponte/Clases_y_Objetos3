aad70455f482acad84aca38476d0613514ddf4a2
dami <damianponte@hotmail.es>
2026-01-22 12:04:59 +0100

    empezando ejercicio

---
+++ src/TarjetaTransporte.java
@@ -0,0 +1,8 @@
+public class TarjetaTransporte {
+    //Crea una clase TarjetaTransporte con un atributo saldo.
+    // Implementa un constructor que reciba el saldo inicial.
+    // Añade un método recargar que aumente el saldo y otro método pagarViaje que reste el coste del viaje solo si hay saldo suficiente.
+    //  Añade un método mostrarSaldo. Desde el main, prueba distintos pagos y recargas.
+
+    
+}
